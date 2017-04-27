6c56504f41d9acc77902bd451a10b1d88b764191
htwright <harrisontwright@gmail.com>
2017-04-27 12:38:30 -0500

    more cleaning

--- server.js
+++ server.js
@@ -1,41 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { DATABASE, PORT } = require('./config');
-
 const app = express();
+let server;
+let knex;
+
 app.use(bodyParser.json());
 
-// ADD EXPRESS MIDDLEWARE FOR CORS HEADERS HERE
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://chai-http.test');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
   res.header('Access-Control-Max-Age', '86400');
   res.header('location', 'trash');
-
   next();
 });
-// app.use((req, res, next)=>{
-// res.header('location', 'trash');
-//   next();
-// });
 
-// app.post('/api/items', (req, res) =>{
-//   res.status(201).json(req.body);
-// })
-// ADD GET POST PUT DELETE ENDPOINTS HERE
 app.get('/api/items', (req, res) => {
   knex('items').select().orderBy('id', 'desc').then((result)=>res.status(200).json(result));
-
 });
 
 app.post('/api/items', (req, res) => {
   knex('items').insert({title:req.body.title}).then(()=>res.status(201));
-
 });
 
-let server;
-let knex;
 function runServer(database = DATABASE, port = PORT) {
   return new Promise((resolve, reject) => {
     try {
