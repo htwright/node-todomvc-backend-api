41348a6cfc2ab03ebcf2a9bb8c7abd673c0b056e
Scylol <djd3dobbin@gmail.com>
2017-04-27 15:45:51 -0400

    added second get and post request

--- server.js
+++ server.js
@@ -8,57 +8,73 @@ let knex;
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://chai-http.test');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-  res.header('Access-Control-Max-Age', '86400');
-  res.header('location', 'trash');
-  next();
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+    res.header('Access-Control-Max-Age', '86400');
+    res.header('location', 'trash');
+    next();
 });
 
 app.get('/api/items', (req, res) => {
-  knex('items').select().orderBy('id', 'desc').then((result)=>res.status(200).json(result));
+    knex('items').select().then((result)=>res.status(200).json(result));
+    
+});
+
+app.get('/api/items/:id', (req, res) => {
+    let {id} = req.params;
+    knex('items').select().where('id', id).then((result)=>res.status(200).json(result));
 });
 
 app.post('/api/items', (req, res) => {
-  knex('items').insert({title:req.body.title}).then(()=>res.status(201));
+    const requiredFields = ['title'];
+    for (let i = 0; i < requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing ${field} in request body`;
+            console.log(message);
+            return res.status(400)
+				.send(message);
+        }
+    }
+    knex('items').insert({title: req.body.title}).returning(['id', 'title']).then((result)=> res.status(201).json(result[0]));
 });
 
 function runServer(database = DATABASE, port = PORT) {
-  return new Promise((resolve, reject) => {
-    try {
-      knex = require('knex')(database);
-      server = app.listen(port, () => {
-        console.info(`App listening on port ${server.address().port}`);
-        resolve();
-      });
-    }
-    catch (err) {
-      console.error(`Can't start server: ${err}`);
-      reject(err);
-    }
-  });
+    return new Promise((resolve, reject) => {
+        try {
+            knex = require('knex')(database);
+            server = app.listen(port, () => {
+                console.info(`App listening on port ${server.address().port}`);
+                resolve();
+            });
+        }
+        catch (err) {
+            console.error(`Can't start server: ${err}`);
+            reject(err);
+        }
+    });
 }
 
 function closeServer() {
-  return knex.destroy().then(() => {
-    return new Promise((resolve, reject) => {
-      console.log('Closing servers');
-      server.close(err => {
-        if (err) {
-          return reject(err);
-        }
-        resolve();
-      });
+    return knex.destroy().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing servers');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
     });
-  });
 }
 
 if (require.main === module) {
-  runServer().catch(err => {
-    console.error(`Can't start server: ${err}`);
-    throw err;
-  });
+    runServer().catch(err => {
+        console.error(`Can't start server: ${err}`);
+        throw err;
+    });
 }
 
 module.exports = { app, runServer, closeServer };
