eec532b7f30bae4a6a1ae577ff81e2dce76a2f1f
htwright <harrisontwright@gmail.com>
2017-04-27 12:28:03 -0500

    limter'd

--- server.js
+++ server.js
@@ -7,16 +7,16 @@ app.use(bodyParser.json());
 
 // ADD EXPRESS MIDDLEWARE FOR CORS HEADERS HERE
 app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', 'http://chai-http.test');
-	res.header('Access-Control-Allow-Headers', 'Content-Type');
-	res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-	res.header('Access-Control-Max-Age', '86400');
-	next();
+  res.header('Access-Control-Allow-Origin', 'http://chai-http.test');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+  res.header('Access-Control-Max-Age', '86400');
+  next();
 });
 app.use((req, res, next)=>{
   res.header('location', 'trash');
   next();
-})
+});
 
 // app.post('/api/items', (req, res) =>{
 //   res.status(201).json(req.body);
@@ -28,47 +28,47 @@ app.get('/api/items', (req, res) => {
 });
 
 app.post('/api/items', (req, res) => {
-  knex('items').insert({title:req.body.title}).then((result)=>res.status(201));
+  knex('items').insert({title:req.body.title}).then(()=>res.status(201));
 
-})
+});
 
 let server;
 let knex;
 function runServer(database = DATABASE, port = PORT) {
-	return new Promise((resolve, reject) => {
-		try {
-			knex = require('knex')(database);
-			server = app.listen(port, () => {
-				console.info(`App listening on port ${server.address().port}`);
-				resolve();
-			});
-		}
-		catch (err) {
-			console.error(`Can't start server: ${err}`);
-			reject(err);
-		}
-	});
+  return new Promise((resolve, reject) => {
+    try {
+      knex = require('knex')(database);
+      server = app.listen(port, () => {
+        console.info(`App listening on port ${server.address().port}`);
+        resolve();
+      });
+    }
+    catch (err) {
+      console.error(`Can't start server: ${err}`);
+      reject(err);
+    }
+  });
 }
 
 function closeServer() {
-	return knex.destroy().then(() => {
-		return new Promise((resolve, reject) => {
-			console.log('Closing servers');
-			server.close(err => {
-				if (err) {
-					return reject(err);
-				}
-				resolve();
-			});
-		});
-	});
+  return knex.destroy().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing servers');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
 }
 
 if (require.main === module) {
-	runServer().catch(err => {
-		console.error(`Can't start server: ${err}`);
-		throw err;
-	});
+  runServer().catch(err => {
+    console.error(`Can't start server: ${err}`);
+    throw err;
+  });
 }
 
 module.exports = { app, runServer, closeServer };
