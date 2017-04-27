d58db1a1070847f89f662a4413a0bea311a24e34
Scylol <djd3dobbin@gmail.com>
2017-04-27 11:30:07 -0400

    working get skeleton

--- server.js
+++ server.js
@@ -6,49 +6,58 @@ const app = express();
 app.use(bodyParser.json());
 
 // ADD EXPRESS MIDDLEWARE FOR CORS HEADERS HERE
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', 'http://chai-http.test');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+	res.header('Access-Control-Max-Age', '86400');
+	next();
+});
+
+
 
 // ADD GET POST PUT DELETE ENDPOINTS HERE
 app.get('/api/items', (req, res) => {
-  res.send();
+	res.status(200).json([]);
 });
 
 let server;
 let knex;
 function runServer(database = DATABASE, port = PORT) {
-  return new Promise((resolve, reject) => {
-    try {
-      knex = require('knex')(database);
-      server = app.listen(port, () => {
-        console.info(`App listening on port ${server.address().port}`);
-        resolve();
-      });
-    }
-    catch (err) {
-      console.error(`Can't start server: ${err}`);
-      reject(err);
-    }
-  });
+	return new Promise((resolve, reject) => {
+		try {
+			knex = require('knex')(database);
+			server = app.listen(port, () => {
+				console.info(`App listening on port ${server.address().port}`);
+				resolve();
+			});
+		}
+		catch (err) {
+			console.error(`Can't start server: ${err}`);
+			reject(err);
+		}
+	});
 }
 
 function closeServer() {
-  return knex.destroy().then(() => {
-    return new Promise((resolve, reject) => {
-      console.log('Closing servers');
-      server.close(err => {
-        if (err) {
-          return reject(err);
-        }
-        resolve();
-      });
-    });
-  });
+	return knex.destroy().then(() => {
+		return new Promise((resolve, reject) => {
+			console.log('Closing servers');
+			server.close(err => {
+				if (err) {
+					return reject(err);
+				}
+				resolve();
+			});
+		});
+	});
 }
 
 if (require.main === module) {
-  runServer().catch(err => {
-    console.error(`Can't start server: ${err}`);
-    throw err;
-  });
+	runServer().catch(err => {
+		console.error(`Can't start server: ${err}`);
+		throw err;
+	});
 }
 
 module.exports = { app, runServer, closeServer };
